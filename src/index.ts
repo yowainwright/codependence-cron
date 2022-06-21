07e29c756d2b85b7ad85407f07e7ec8ce3780d7e
yowainwright <yowainwright@gmail.com>
2022-06-21 16:59:21 -0700

    feat: setup build pipeline

---
+++ src/index.ts
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+
+import { program } from 'commander'
+import { readFileSync, writeFileSync } from 'fs-extra'
+
+import { ConstructVersionMapOptions, PackageJson } from './types'
+
+export const updateCodependencies = ({
+	packageJson = 'package.json',
+	rootDir: cwd = './',
+	debug = false,
+}: ConstructVersionMapOptions = {}): void => {
+	const rootPkgPath = `${cwd}${packageJson}`
+	const rootPkg = readFileSync(`${cwd}${packageJson}`, 'utf8')
+	const json = JSON.parse(rootPkg)
+	const { dependencies = {}, devDependencies = {} } = json as PackageJson
+	const codependencies = Object.keys(dependencies).concat(
+		Object.keys(devDependencies),
+	)
+	const updatedJson = {
+		...json,
+		codependence: {
+			codependencies,
+		},
+	}
+	if (debug) console.log({ updatedJson })
+	writeFileSync(rootPkgPath, JSON.stringify(updatedJson, null, 2).concat('\n'))
+}
+
+program
+	.description(
+		'Codependency cron, constructs codependencies from dependencies and devDependencies',
+	)
+	.option('-p, --packageJson', 'dependencies json reference')
+	.option('-r, --rootDir', 'directory of json reference')
+	.option('--debug', 'enable debugging')
+	.action(updateCodependencies)
+	.parse(process.argv)
+
+export { program }
---
+++ src/types.ts
@@ -0,0 +1,10 @@
+export type ConstructVersionMapOptions = {
+	packageJson?: string
+	rootDir?: string
+	debug?: boolean
+}
+
+export type PackageJson = {
+	dependencies?: Record<string, string>
+	devDependencies?: Record<string, string>
+}
